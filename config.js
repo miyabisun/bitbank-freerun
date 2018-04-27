5d7971c6a6a1a4982d84a4671bc3525f9b0e4f8d
miyabisun <miyabi.ooh@gmail.com>
2018-04-27 22:22:39 +0900

    mv config.js to config.ls

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PAIR: process.env.PAIR || 'btc_jpy',
-  API_KEY: process.env.API_KEY || '',
-  API_SECRET: process.env.API_SECRET || '',
-  LIMIT: parseInt(process.env.LIMIT || 3),
-}
-
